80c74fe08b70db94a92976d0d6d9be206f8eb4ef
murphy <murphyqq1@gmail.com>
2022-06-28 14:06:11 +0800

    fix: add cookie to header

--- movie/fetch.go
+++ movie/fetch.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-func GetMovieBody(start int) string {
+func GetMovieBodyFromStart(start int) string {
 	//url := "https://movie.douban.com/top250"
 	url := ChooseStartId(start)
 	method := "GET"
@@ -20,7 +20,9 @@ func GetMovieBody(start int) string {
 		log.Println(err)
 
 	}
-	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")
+	// 此cookie是登录豆瓣后从浏览器获得，因为有时间限制，需要勤换
+	req.Header.Add("cookie", "ll=\"108288\"; bid=tfmu0VNtswM; douban-fav-remind=1; ct=y; viewed=\"26979890_30357170_30397714_27663285_30389976_25782902_19952400_35410754_35668581_30395692\"; push_noty_num=0; push_doumail_num=0; ap_v=0,6.0; dbcl2=\"156810236:67LMVXkh0Sw\"; ck=1CSY")
 
 	resp, err := client.Do(req)
 	if err != nil {
